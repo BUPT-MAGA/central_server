913da97bf6f2aacb0b299de5918233b138cc2b88
jasonzhnag <zhangbeichen819@163.com>
2021-06-02 00:21:56 +0800

    FEA: add empty route

--- central_server/api/center.py
+++ central_server/api/center.py
@@ -1,6 +1,41 @@
-from flask import Blueprint, Flask
+from flask import Blueprint, Flask, request
+from flask_login import login_user
+import json
 
 def add_center_routes(app: Flask):
-    @app.route('/')
-    def hello():
-        return 'hello world'
+    @app.route('/manager/login', methods=['POST'])
+    def login():
+        if 'username' in request.form and 'password' in request.form:
+            username = request.form['username']
+            password = request.form['password']
+            # TODO
+            # https://github.com/PrettyPrinted/flask_auth_scotch/blob/master/project/auth.py
+            # login_user()
+
+    @app.route('/manager/register', methods=['POST'])
+    def register():
+        if 'username' in request.form and 'password' in request.form:
+            username = request.form['username']
+            password = request.form['password']
+            # TODO
+            # https://github.com/PrettyPrinted/flask_auth_scotch/blob/master/project/auth.py
+            # login_user()
+
+    @app.route('/air/switch', methods=['GET'])
+    def switch_air():
+        action = int(request.args.get('action', 0))
+        # TODO handle switch
+        return 'switch successfully'
+
+    @app.route('/air/mode', methods=['GET'])
+    def set_mode():
+        mode = int(request.args.get('mode', 0))
+        # TODO handle setting mode
+        return 'set mode successfully'
+
+    @app.route('/air/statistic', methods=['GET'])
+    def get_statistic():
+        roomId = request.args.get('roomId', '')
+        scale = request.args.get('scale', 0)
+        # TODO handle getting statistic
+        return json.dumps(dict())
