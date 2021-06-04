a1276a4a25c83f048d8685ab69e8d8bfc35f4faa
Yichen Xu <yichen.x@outlook.com>
2021-06-04 21:58:20 +0800

    Update AdminModel

--- central_server/io/admin.py
+++ central_server/io/admin.py
@@ -1,6 +1,7 @@
 from .data_model import DataModel
+from central_server.models.admin import Admin
 
 
 class AdminData(DataModel):
     def __init__(self, db_path: str) -> 'AdminData':
-        super().__init__(db_path, 'admin', 'username')
+        super().__init__(db_path, 'admin', 'username', Admin)
