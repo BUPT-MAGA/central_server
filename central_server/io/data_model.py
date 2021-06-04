e04387e6330e503ba5e007dd6ba0fd645aba2b04
Yichen Xu <yichen.x@outlook.com>
2021-06-04 21:52:14 +0800

    Update DataModel

--- central_server/io/data_model.py
+++ central_server/io/data_model.py
@@ -1,18 +1,24 @@
 from typing import NamedTuple, Callable
 from tinydb import TinyDB, Query
 
+from .db_instance import DBInstance
+
 
 class DataModel(object):
     def init_db(db_path: str):
-        return TinyDB(db_path)
+        db_inst = DBInstance()
+        return db_inst.get_db(db_path)
 
     Data = Query()
 
-    def __init__(self, db_path: str, data_name: str, primary_key: str):
+    def __init__(self,
+                 db_path: str,
+                 data_name: str, primary_key: str, constructor: type):
         self.db = self.init_db(db_path)
         self.data_name = data_name
         self.table = self.db.table(self.data_name)
         self.primary_key = primary_key
+        self.constructor = constructor
 
     def insert(self, data: NamedTuple):
         self.table.insert(data._asdict())
@@ -22,7 +28,8 @@ class DataModel(object):
         if len(res) == 0:
             return None
         else:
-            return res[0]
+            res = res[0]
+            return self.constructor(**res)
 
     def update(self, pid, field_name: str, value):
         self.table.update({field_name: value}, Data[self.primary_key] == pid)
