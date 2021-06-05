2821ede14877315f55746ad21b33232ffdbd6c27
Yichen Xu <yichen.x@outlook.com>
2021-06-05 16:37:07 +0800

    Refactor enum types to avoid cyclic reference

---
+++ central_server/models/types.py
@@ -0,0 +1,30 @@
+from enum import IntEnum
+
+
+class EventType(IntEnum):
+    TEMP = 1
+    START = 2
+    END = 3
+
+
+class CheckInStatus(IntEnum):
+    CheckIn = 1
+    CheckOut = 2
+
+
+class AdminStatus(IntEnum):
+    Off = 1
+    On = 2
+
+
+class WindMode(IntEnum):
+    Snow = 1
+    Sun = 2
+
+
+class WindSpeed(IntEnum):
+    Low = 1
+    Mid = 2
+    High = 3
+
+
