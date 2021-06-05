2a8a488f229ad494854cc455af69cdb9de99ac61
jasonzhnag <zhangbeichen819@163.com>
2021-06-05 15:04:05 +0800

    TEST: test queue

--- central_server/models/room.py
+++ central_server/models/room.py
@@ -13,6 +13,11 @@ class WindSpeed(Enum):
     Mid = 2
     High = 3
 
+    def __lt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value < other.value
+        return NotImplemented
+
 
 class Room(BaseModel):
     # Room ID
